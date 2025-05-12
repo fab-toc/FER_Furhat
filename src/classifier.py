30839c8b8a5e0d71b067dea792dd04f7790f37a9
fab-toc <81991643+fab-toc@users.noreply.github.com>
2025-05-13 00:47:24 +0200

    changed the way of downloading dataset

---
+++ src/classifier.py
@@ -0,0 +1,122 @@
+import copy
+from typing import Callable, Optional
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+
+
+def train_classifier(
+    model: nn.Module,
+    train_dataloader: DataLoader,
+    batch_size: int,
+    num_epochs: int,
+    loss_fn: nn.Module,
+    learning_rate: float,
+    device: torch.device,
+    transform_fn: Optional[Callable] = None,
+    verbose: bool = True,
+):
+    # Make a copy of the model (avoid changing the model outside this function)
+    model_tr = copy.deepcopy(model)
+
+    # Move the model to the specified device (GPU or CPU)
+    model_tr = model_tr.to(device)
+
+    # Set the model to training mode, this is important for models that have layers like dropout or batch normalization
+    model_tr.train()
+
+    # Initialize the optimizer
+    optimizer = torch.optim.Adam(model_tr.parameters(), lr=learning_rate)
+
+    # Initialize a list for storing the training loss over epochs
+    train_losses = []
+
+    # Training loop
+    for epoch in range(num_epochs):
+        # Initialize the training loss for the current epoch
+        tr_loss = 0
+
+        # Iterate over batches using the dataloader
+        for images, labels in train_dataloader:
+            images = images.to(device)
+            labels = labels.to(device)
+
+            # If a transform function is provided, apply it to the images
+            if transform_fn is not None:
+                images = transform_fn(images)
+            # TO DO: write the training procedure for each batch. This should consist of:
+            # - vectorizing the images (size should be (batch_size, input_size))
+            images = images.view(batch_size, -1)
+
+            # - calculate the predicted labels from the images using 'model_tr'
+            labels_pred = model_tr(images)
+
+            # - using loss_fn, calculate the 'loss' between the predicted and true labels
+            loss = loss_fn(labels_pred, labels)
+
+            # - set the optimizer gradients at 0 for safety
+            optimizer.zero_grad()
+
+            # - compute the gradients (use the 'backward' method on 'loss')
+            loss.backward()
+
+            # - apply the gradient descent algorithm (perform a step of the optimizer)
+            optimizer.step()
+
+            # Update the current epoch loss
+            # Note that 'loss.item()' is the loss averaged over the batch, so multiply it with the current batch size to get the total batch loss
+            tr_loss += loss.item() * batch_size
+
+        # At the end of each epoch, get the average training loss and store it
+        tr_loss = tr_loss / (len(train_dataloader) * batch_size)
+        train_losses.append(tr_loss)
+
+        # Display the training loss
+        if verbose:
+            print(
+                "Epoch [{}/{}], Training loss: {:.4f}".format(
+                    epoch + 1, num_epochs, tr_loss
+                )
+            )
+
+    return model_tr, train_losses
+
+
+def eval_classifier(
+    model: nn.Module,
+    eval_dataloader: DataLoader,
+    device: torch.device,
+    transform_fn: Optional[Callable] = None,
+):
+    # Set the model in 'evaluation' mode (this disables some layers (batch norm, dropout...) which are not needed when testing)
+    model.eval()
+
+    # In evaluation phase, we don't need to compute gradients (for memory efficiency)
+    with torch.no_grad():
+        # initialize the total and correct number of labels to compute the accuracy
+        correct_labels = 0
+        total_labels = 0
+
+        # Iterate over the dataset using the dataloader
+        for images, labels in eval_dataloader:
+            images = images.to(device)
+            labels = labels.to(device)
+
+            # If a transform function is provided, apply it to the images
+            if transform_fn is not None:
+                images = transform_fn(images)
+
+            # Get the predicted labels
+            y_predicted = model(images)
+
+            # To get the predicted labels, we need to get the max over all possible classes
+            _, labels_predicted = torch.max(y_predicted.data, 1)
+
+            # Compute accuracy: count the total number of samples, and the correct labels (compare the true and predicted labels)
+            total_labels += labels.size(0)
+            correct_labels += (labels_predicted == labels).sum().item()
+
+    accuracy = 100 * correct_labels / total_labels
+
+    return accuracy
