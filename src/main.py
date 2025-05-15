2eefdaacfc84f0116877a4f6d916f6b4ab6acbb8
Fabio Tocco <ftocco@OMEN-10.innovlab>
2025-05-15 12:58:32 +0200

    new test script and start of main

---
+++ src/main.py
@@ -0,0 +1,205 @@
+import pyrealsense2 as rs
+import numpy as np
+import cv2
+import time
+from torch.utils.data import Dataset, DataLoader
+import torch
+import torchvision.transforms as transforms
+import copy
+import os
+import shutil
+from typing import Literal
+
+import kagglehub
+import torch
+import torch.nn as nn
+import torchvision
+from torch.utils.data import DataLoader, random_split
+from train.utils import (
+    eval_classifier,
+    get_data_transforms,
+    get_model,
+    save_model,
+    train_classifier_with_validation,
+)
+
+# class InMemoryFaceDataset(Dataset):
+#     def __init__(self, image_list, transform=None):
+#         self.image_list = image_list
+#         self.transform = transform
+
+#     def __len__(self):
+#         return len(self.image_list)
+
+#     def __getitem__(self, idx):
+#         image = self.image_list[idx]
+
+#         # OpenCV -> PIL
+#         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+#         image = transforms.ToPILImage()(image)
+
+#         if self.transform:
+#             image = self.transform(image)
+
+#         return image
+
+
+
+# === Chargement du classificateur de visage ===
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+if face_cascade.empty():
+    print("❌ Échec du chargement du détecteur de visage.")
+    exit(1)
+
+# === Initialisation de la caméra RealSense ===
+pipeline = rs.pipeline()
+config = rs.config()
+config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 60)
+pipeline.start(config)
+
+# === Créer UNE SEULE fenêtre ===
+cv2.namedWindow("Visages détectés", cv2.WINDOW_NORMAL)
+
+output_dir = "test"
+os.makedirs(output_dir, exist_ok=True)
+
+# === Initialisation du timer ===
+last_capture_time = time.time()
+
+print("✅ Caméra en cours. Appuie sur 'q' pour quitter.")
+
+try:
+    while True:
+        frames = pipeline.wait_for_frames()
+        color_frame = frames.get_color_frame()
+        if not color_frame:
+            continue
+
+        # Image couleur
+        color_image = np.asanyarray(color_frame.get_data())
+        gray = cv2.cvtColor(color_image, cv2.COLOR_BGR2GRAY)
+
+        # Détection de visages
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+
+        # Si des visages sont détectés
+        current_time = time.time()
+        if len(faces) > 0:
+            for i, (x, y, w, h) in enumerate(faces):
+
+                # Capturer toutes les 0.5 secondes
+                if current_time - last_capture_time >= 0.5:
+                    face_crop = color_image[y:y+h, x:x+w].copy()
+                    timestamp = time.strftime("%Y%m%d-%H%M%S")
+                    filename = os.path.join(output_dir, f"face_{timestamp}_{i}.jpg")
+                    cv2.imwrite(filename, face_crop)
+                    print(f"📸 Visage enregistré : {filename}")
+                    last_capture_time = current_time
+
+                # Dessiner le rectangle vert
+                cv2.rectangle(color_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        # Affichage
+        cv2.imshow("Visages détectés", color_image)
+
+        # Sortie avec la touche 'q'
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+finally:
+    pipeline.stop()
+    cv2.destroyAllWindows()
+    print("🧼 Caméra arrêtée.")
+
+
+
+# # Set up the script
+# args = parse_args()
+
+# Set the random seed for reproducibility
+torch.manual_seed(0)
+
+# # Automatic configuration of PyTorch settings
+# hw_info, optimal_params = setup_pytorch_optimal(verbose=True)
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+torch.backends.cuda.matmul.allow_tf32 = True
+torch.backends.cudnn.benchmark = True
+torch.backends.cudnn.allow_tf32 = True
+
+
+######################## HYPER PARAMETERS ########################
+emotions_to_exclude = ["surprise", "neutral", "disgust"]
+
+# Use the optimal parameters
+num_workers = 10
+prefetch_factor = 4
+augmentation_level = "heavy"  # Options: "none", "light", "medium", "heavy"
+
+model_name: Literal["vgg", "convnext"] = "convnext"
+model_version: Literal["11", "13", "16", "19", "tiny", "small", "base", "large"] = (
+    "tiny"
+)
+
+batch_size = 16
+num_epochs: int = 20
+learning_rate: float = 1e-4
+loss_fn: nn.Module = nn.CrossEntropyLoss()
+
+unfreeze_feature_layer_start: int = (
+    2  # Unfreeze the feature layers starting from this one
+)
+
+model_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), f"../trained/{model_name}_{model_version}_b{512}_l{unfreeze_feature_layer_start}_end_e{num_epochs}.pt")
+
+
+data_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "data")
+
+data_transforms = get_data_transforms(
+    input_format="rgb",
+    target_channels=3,  # Target channels for the model
+    target_size=(224, 224),  # Target size of each image for the model used
+    augmentation_level=augmentation_level,
+    custom_means=[0.485, 0.456, 0.406],  # ImageNet stats by default
+    custom_stds=[0.229, 0.224, 0.225],  # ImageNet stats by default
+)
+
+
+
+
+
+# Load the dataset
+train_data = torchvision.datasets.ImageFolder(
+    os.path.join(data_dir, output_dir),
+    transform=data_transforms,
+)
+
+
+test_dataloader = DataLoader(train_data, batch_size=8, shuffle=True,drop_last=True,num_workers=num_workers,pin_memory=True,persistent_workers=True,prefetch_factor=prefetch_factor,)
+
+
+# Get the model based on the hyperparameters
+model = get_model(
+    model_name=model_name,
+    model_version=model_version,
+    num_classes=4,
+    unfreeze_feature_layer_start=unfreeze_feature_layer_start,
+)
+
+# Print model structure to understand what we're working with
+print("\nModel structure:")
+print(model, "\n")
+
+
+######################## TESTING ########################
+model_test = copy.deepcopy(model)
+model_test.load_state_dict(torch.load(model_path))
+
+# - Apply the evaluation function using the test dataloader
+test_accuracy, avg_loss = eval_classifier(
+    model=model_test, eval_dataloader=test_dataloader, device=device, loss_fn=loss_fn
+)
+
+print("======== TEST RESULTS ========")
+print("Test accuracy: {:.2f}%".format(test_accuracy))
+print("Average loss: {:.4f}".format(avg_loss))
---
+++ src/test.py
@@ -0,0 +1,164 @@
+import copy
+import os
+from typing import Literal
+
+import torch
+import torch.nn as nn
+import torchvision
+from torch.utils.data import DataLoader
+from train.utils import (
+    eval_classifier,
+    get_data_transforms,
+    get_model,
+)
+
+# # Set up the script
+# args = parse_args()
+
+# Set the random seed for reproducibility
+torch.manual_seed(0)
+
+# # Automatic configuration of PyTorch settings
+# hw_info, optimal_params = setup_pytorch_optimal(verbose=True)
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+torch.backends.cuda.matmul.allow_tf32 = True
+torch.backends.cudnn.benchmark = True
+torch.backends.cudnn.allow_tf32 = True
+
+
+######################## HYPER PARAMETERS ########################
+emotions_to_exclude = ["surprise", "neutral", "disgust"]
+
+# Use the optimal parameters
+num_workers = 10
+prefetch_factor = 4
+augmentation_level = "heavy"  # Options: "none", "light", "medium", "heavy"
+
+model_name: Literal["vgg", "convnext"] = "convnext"
+model_version: Literal["11", "13", "16", "19", "tiny", "small", "base", "large"] = (
+    "tiny"
+)
+
+batch_size = 16
+num_epochs: int = 20
+learning_rate: float = 1e-4
+loss_fn: nn.Module = nn.CrossEntropyLoss()
+
+unfreeze_feature_layer_start: int = (
+    2  # Unfreeze the feature layers starting from this one
+)
+
+model_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), f"../trained/{model_name}_{model_version}_b{512}_l{unfreeze_feature_layer_start}_end_e{num_epochs}.pt")
+
+# Print all hyperparameters for verification
+print("\n======== HYPERPARAMETERS ========")
+print(f"Emotions to exclude: {emotions_to_exclude}")
+print(f"Augmentation level: {augmentation_level}")
+print(f"Model name: {model_name}")
+print(f"Model version: {model_version}")
+print(f"Batch size: {batch_size}")
+print(f"Number of epochs: {num_epochs}")
+print(f"Learning rate: {learning_rate}")
+print(f"Unfreeze feature layers starting from: {unfreeze_feature_layer_start}")
+print(f"Number of workers: {num_workers}")
+print(f"Prefetch factor: {prefetch_factor}")
+print(f"Device: {device}")
+print("================================\n")
+
+
+######################## DATASET ########################
+data_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "data")
+
+data_transforms = get_data_transforms(
+    input_format="rgb",
+    target_channels=3,  # Target channels for the model
+    target_size=(224, 224),  # Target size of each image for the model used
+    augmentation_level=augmentation_level,
+    custom_means=[0.485, 0.456, 0.406],  # ImageNet stats by default
+    custom_stds=[0.229, 0.224, 0.225],  # ImageNet stats by default
+)
+
+
+# Load the dataset
+train_data = torchvision.datasets.ImageFolder(
+    os.path.join(data_dir, "train"),
+    transform=data_transforms,
+)
+
+test_data = torchvision.datasets.ImageFolder(
+    os.path.join(data_dir, "test"),
+    transform=data_transforms,
+)
+
+CLASSES = train_data.classes
+print("Classes of the dataset:", CLASSES)
+
+# Print the mapping of labels to emotions
+print("\nMapping of labels to emotions:")
+for idx, emotion in enumerate(CLASSES):
+    print(f"Label {idx} → {emotion}")
+
+
+print(f"\nNumber of train samples: {len(train_data)}")
+print(f"Number of test samples: {len(test_data)}")
+
+
+######################## DATALOADERS ########################
+train_dataloader = DataLoader(
+    train_data,
+    batch_size=batch_size,
+    shuffle=True,
+    drop_last=True,
+    num_workers=num_workers,
+    pin_memory=True,
+    persistent_workers=True,
+    prefetch_factor=prefetch_factor,
+)
+
+
+test_dataloader = DataLoader(
+    test_data,
+    batch_size=batch_size,
+    shuffle=True,
+    drop_last=True,
+    num_workers=num_workers,
+    pin_memory=True,
+    persistent_workers=True,
+    prefetch_factor=prefetch_factor,
+)
+
+num_batches = len(train_dataloader)
+print("Number of batches in the training subset:", num_batches)
+
+
+num_batches = len(test_dataloader)
+print("Number of batches in the testing subset:", num_batches)
+
+
+######################## MODEL ########################
+# Get the model based on the hyperparameters
+model = get_model(
+    model_name=model_name,
+    model_version=model_version,
+    num_classes=len(CLASSES),
+    unfreeze_feature_layer_start=unfreeze_feature_layer_start,
+)
+
+# Print model structure to understand what we're working with
+print("\nModel structure:")
+print(model, "\n")
+
+
+######################## TESTING ########################
+model_test = copy.deepcopy(model)
+model_test.load_state_dict(torch.load(model_path))
+
+# - Apply the evaluation function using the test dataloader
+test_accuracy, avg_loss = eval_classifier(
+    model=model_test, eval_dataloader=test_dataloader, device=device, loss_fn=loss_fn
+)
+
+print("======== TEST RESULTS ========")
+print("Test accuracy: {:.2f}%".format(test_accuracy))
+print("Average loss: {:.4f}".format(avg_loss))
